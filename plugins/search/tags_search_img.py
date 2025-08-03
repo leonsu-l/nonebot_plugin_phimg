71c39d6bc88a4246abf3a3ce1345079f56022d7b
jessdaodao <694428809@qq.com>
2025-08-03 20:37:56 +0800

    为视频和图片分别采用不同的压缩等级

--- plugins/search/tags_search_img.py
+++ plugins/search/tags_search_img.py
@@ -15,6 +15,13 @@ def select_random_img(imgs_list: list[dict]) -> dict:
     """已有列表中随机选一个图片"""
     img_index = randint(0, len(imgs_list) - 1)
     selected_img = imgs_list[img_index]
+    file = selected_img["representations"]["full"]
+
+    if file.lower().endswith('.webm'):
+        return {
+            "url": selected_img["representations"]["medium"],
+            "score": selected_img["score"]
+        }
 
     return {
         "url": selected_img["representations"]["large"],

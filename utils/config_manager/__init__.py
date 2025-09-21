6168667e7b4194a81b510762a5a4e11e1c4fcd57
JessDaodao <694428809@qq.com>
2025-09-21 12:32:15 +0800

    现在插件会自动补全config文件夹

--- utils/config_manager/__init__.py
+++ utils/config_manager/__init__.py
@@ -1,9 +1,13 @@
 from pathlib import Path
+import os
 
 from .global_manager import GlobalConfigManager
 from .group_manager import GroupConfigManager
 
 CONFIG_PATH = (Path(__file__).parent.parent.parent / "config").resolve()
+
+if not CONFIG_PATH.exists():
+    os.makedirs(CONFIG_PATH, exist_ok=True)
 
 __global_config_json = str(CONFIG_PATH / "global_config.json")
 global_cfg = GlobalConfigManager(__global_config_json)
